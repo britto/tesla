f5b0c285191b9a003f30a67aae4fda03730a461f
João Britto <jabcalves@gmail.com>
2020-09-01 18:14:36 -0300

    Dummy Timeout middleware for debugging purposes

--- lib/tesla/middleware/timeout.ex
+++ lib/tesla/middleware/timeout.ex
@@ -19,45 +19,8 @@ defmodule Tesla.Middleware.Timeout do
 
   @behaviour Tesla.Middleware
 
-  @default_timeout 1_000
-
   @impl Tesla.Middleware
-  def call(env, next, opts) do
-    opts = opts || []
-    timeout = Keyword.get(opts, :timeout, @default_timeout)
-
-    task = safe_async(fn -> Tesla.run(env, next) end)
-
-    try do
-      task
-      |> Task.await(timeout)
-      |> repass_error
-    catch
-      :exit, {:timeout, _} ->
-        Task.shutdown(task, 0)
-        {:error, :timeout}
-    end
+  def call(env, next, _opts) do
+    Tesla.run(env, next)
   end
-
-  defp safe_async(func) do
-    Task.async(fn ->
-      try do
-        {:ok, func.()}
-      rescue
-        e in _ ->
-          {:exception, e, System.stacktrace()}
-      catch
-        type, value ->
-          {type, value}
-      end
-    end)
-  end
-
-  defp repass_error({:exception, error, stacktrace}), do: reraise(error, stacktrace)
-
-  defp repass_error({:throw, value}), do: throw(value)
-
-  defp repass_error({:exit, value}), do: exit(value)
-
-  defp repass_error({:ok, result}), do: result
 end
